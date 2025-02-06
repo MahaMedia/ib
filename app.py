ab6060eb75c821e4491c4d6e6a6fdb20f94cf697
JR <jr@mahamedia.us>
2025-02-06 14:13:49 -0600

    added headless playwright browser functionality by running via subprocess

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, jsonify
+from test import test
+import os
+import subprocess
+from playwright.sync_api import sync_playwright
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     return render_template('index.template')
+    
+@app.route('/test', methods=['GET'])
+def show_test_script():
+    try:
+        result = subprocess.run(
+            ["/var/www/mahamedia.us/intelligencebank_tools/venv/bin/python3",
+             "/var/www/mahamedia.us/intelligencebank_tools/test.py"],
+            capture_output=True,
+            text=True
+        )
+
+        if result.returncode != 0:
+            return jsonify({"error": result.stderr}), 500
+
+        return jsonify({"message": "Test executed successfully", "output": result.stdout.strip()})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="http://195.250.25.177/")
--- app.wsgi
+++ app.wsgi
@@ -1,6 +1,8 @@
 import sys
 import os
 
+os.environ["PLAYWRIGHT_BROWSERS_PATH"] = "/var/www/mahamedia.us/intelligencebank_tools/venv/browser_binaries"
+
 venv_path = "/var/www/mahamedia.us/ib/venv"
 sys.path.insert(0, os.path.join(venv_path, "lib/python3.12/site-packages"))
 sys.path.insert(0, "/var/www/mahamedia.us/ib")
